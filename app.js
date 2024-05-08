4f36966b934745616f735b82c546d85d46a337e9
jeeva m <jeelion22@gmail.com>
2024-05-08 17:25:22 +0530

    comment out

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
     ],
-    methods: ["GET", "POST", "PUT"],
+    // methods: ["GET", "POST", "PUT"],
     credentials: true,
   })
 );
